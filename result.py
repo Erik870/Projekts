94133046386ec104c8a8e65f4f4614a891fd6026
Eriks_Baumanis_231RDC032 <144455774+Erik870@users.noreply.github.com>
2023-12-28 13:59:26 +0000

    Pievieno kodu, kurš izrtulko latviešu valoda, un saglaba "Translations[] " masīvā

--- result.py
+++ result.py
@@ -14,9 +14,9 @@ service = Service()
 driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 10)
 
-words=[]            # angļu valodas vārdi 
-Translations = []   # latviešu valodas vārdi
-Translations_2 = [] # krievu valodas vārdi
+words=[]                # angļu valodas vārdi 
+Translations = []       # latviešu valodas vārdi
+Translations_2 = []     # krievu valodas vārdi
 
 fails = pandas.read_excel("Words.xlsx", sheet_name="Sheet1")
 info_list = fails.values.tolist()
@@ -25,3 +25,33 @@ for row in info_list:           #paņēm vārdus no "Sheet1" un ievadā masīvā
     words.append(row[0])
 
 words.sort() # sakārto pēc alfabētas secība
+
+##print(words)
+
+#Tulkotāja "URL", kurš tulko latviešu valodā 
+
+url = "https://translate.yandex.com/en/?source_lang=en&target_lang=lv"
+
+
+for word in words:          #Cikls kurš partilko vārdus uz latviešu valodu
+    
+    driver.get(url)         # saņēm "URL"
+
+    input_field=driver.find_element(By.XPATH, "//*[@id='fakeArea']")            #atrod lauku kur jāievada vārdu
+
+    time.sleep(2)           #aizkavēšana, lai tīmēkļa lapa domātu, ka mēs esam cilvēks                        
+    
+    input_field.clear()             #notirā lauku
+    input_field.send_keys(word)     #ievieto vārdu laukā
+    
+    time.sleep(2)
+
+    translated_text = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'translation')]"))).text   #nokopē pārtulkoto vārdu ar aizkavēšanu 
+
+    
+    print(translated_text)  #izdruka pārtulkotu vārdu, kurš tikko boja iekopēts
+    
+    Translations.append(translated_text)    #pievieno pārtulkotu vārdu masīvā "Translations[]"
+
+
+driver.close()
